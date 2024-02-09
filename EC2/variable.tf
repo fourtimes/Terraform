6af94676d9327f9059126470778bdf1a06081001
fourtimes <91359308+fourtimes@users.noreply.github.com>
2024-02-09 17:22:42 +0530

    Update variable.tf

--- EC2/variable.tf
+++ EC2/variable.tf
@@ -1,10 +1,10 @@
 variable "AWS_REGION" {
-    default = "us-east-2"
+    default = "xxxxxxx" # pass your region
 }
 variable "AWS_ACCESS_KEY" {
-  default="AKIA6E4DZ2CFOKEA6IPL"
+  default="xxxxxxxxxxxxx" # pass your access key
 }
 
 variable "AWS_SECRET_KEY" {
-  default="pJDc5R5QJuCKmNJmZvaawEvXCq1K4qyZYbDUCPqj"
+  default="xxxxxxxxxxxxxxx"  # pass your secret key
 }
